c7286c342b31ec5c20ad01169256eb5f06712fd3
dyox <dyox@965621fb-cb66-4b2d-8d06-0639b064d873>
2011-07-22 12:53:35 +0000

    [Framework]
    ++ Added Utils files

---
+++ FrameWork/Utils/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrameWork
+{
+    static public class Utils
+    {
+        static public List<T> ConvertStringToArray<T>(string Value)
+        {
+            string[] Values = Value.Split(' ');
+            List<T> L = new List<T>();
+            foreach (string Val in Values)
+                if (Val.Length > 0)
+                    L.Add((T)Convert.ChangeType(Val, typeof(T)));
+
+            return L;
+        }
+        static public string ConvertArrayToString<T>(T[] Value)
+        {
+            string Result = "";
+            foreach (T val in Value)
+                Result += (string)Convert.ChangeType(val, typeof(string)) + " ";
+
+            return Result;
+        }
+    }
+}
